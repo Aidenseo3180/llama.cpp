3aa69c903397a1bfda5252bdfd3c20a5b6d08c3d
Aiden <minwoo3180@gmail.com>
2025-11-08 20:09:28 -0500

    testing 23-28

--- src/llama-context.cpp
+++ src/llama-context.cpp
@@ -1065,8 +1065,8 @@ int llama_context::decode(const llama_batch & batch_inp) {
         } else {
             if (!is_generating) {
                 if (1 < 100) {
-                    printf("using LLAMA_SKIP_25_28");
-                    skip_mode = LLAMA_SKIP_25_28;
+                    // printf("using LLAMA_SKIP_25_28");
+                    skip_mode = LLAMA_SKIP_23_28;
                 } else {
                     skip_mode = LLAMA_SKIP_25_28;
                 }
